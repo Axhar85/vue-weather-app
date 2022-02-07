44328241a6ce7f8559e5bc27a43f0ad9420ab7cc
Axhar <azhararshad_ntu@yahoo.com>
2022-02-07 12:22:41 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -54,7 +54,7 @@ export default {
       let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
       let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
       
-      et day = days[d.getDay()];
+      let day = days[d.getDay()];
       let date = d.getDate();
       let month = months[d.getMonth()];
       let year = d.getFullYear();
